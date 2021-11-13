20450b41b66acf3c1f360feb305807acc73167e1
unknown <dc.wang840533@gmail.com>
2021-11-13 14:36:14 -0500

    laptop copy

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,14 +7,14 @@ const STYLES= ['btn--primary', 'btn--outline']
 const SIZES= ['btn--medium', 'btn--large'];
 
 interface Props{
-    children?: any,
-    // type?: any,
-    // onClick?: any,
+    children?: string,
+    type?: any,
+    onClick?: any,
     buttonStyle?: any,
     buttonSize?: any
 }
 
-const Button:React.FC<Props> = ({children, buttonStyle, buttonSize}) => {
+export const Button:React.FC<Props> = ({children, onClick,type, buttonStyle, buttonSize}) => {
     //check if it has a style if it does then keep it the way it is if not set to btn--primary
     const checkButtonStyles= STYLES.includes(buttonStyle) ? buttonStyle : STYLES[0];
     //you can also just write it in className but its cleaner
@@ -25,12 +25,13 @@ const Button:React.FC<Props> = ({children, buttonStyle, buttonSize}) => {
         <Link to= '/sign-up' className= 'btn-mobile'>
             {/* rendering whatever you passed in as the prop */}
             {/* two main className styles of the button */}
-            <button className={`btn ${checkButtonStyles} ${checkButtonSize}`}>
-                {/* onClick={onClick} */}
+            <button className={`btn ${checkButtonStyles} ${checkButtonSize}`}
+            onClick= {onClick}
+            type= {type}
+            >
                 {children}
             </button>
         </Link>
     )
 }
 
-export default Button
