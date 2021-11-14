59468d021656f0e9919d584de25d0b25ba328339
unknown <dc.wang840533@gmail.com>
2021-11-14 12:13:03 -0500

    routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import "./App.css";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./components/pages/Home";
-
+import Games from "./components/pages/Games";
+import Tech from "./components/pages/Tech";
+import SignUp from "./components/pages/SignUp";
 //different syntax?
 // const App: React.FC = () => {
 
@@ -15,7 +17,10 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path= "/" exact component= {Home}/>
+          <Route path= "/home" exact component= {Home}/>
+          <Route path= "/games" exact component= {Games}/>
+          <Route path= "/tech" exact component= {Tech}/>
+          <Route path= "/sign-up" exact component= {SignUp}/>
         </Switch>
       </Router>
     </>
---
+++ src/components/pages/Games.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../App.css'
+const Games:React.FC = () => {
+    return (
+        <div className="games">
+            <h1 className="product-text">GAMES</h1>
+        </div>
+    )
+}
+
+export default Games
