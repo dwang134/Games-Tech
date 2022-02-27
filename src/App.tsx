9e4645153f5c06645c3a2ec8135470ce470f7569
DEELIN WONG <dwang134@DEELINs-MacBook-Pro.local>
2022-02-27 14:30:06 -0500

    pages fix

--- src/App.tsx
+++ src/App.tsx
@@ -17,11 +17,11 @@ function App() {
       <Router>
         <Navbar />
         <Switch>
-          <Route path= "/" exact component= {Home}/>
-          <Route path= "/home" exact component= {Home}/>
-          <Route path= "/games" exact component= {Games}/>
-          <Route path= "/tech" exact component= {Tech}/>
-          <Route path= "/sign-up" exact component= {SignUp}/>
+          <Route path= "/Games-Tech" exact component= {Home}/>
+          <Route path= "/Games-Tech/home" exact component= {Home}/>
+          <Route path= "/Games-Tech/games" exact component= {Games}/>
+          <Route path= "/Games-Tech/tech" exact component= {Tech}/>
+          <Route path= "/Games-Tech/sign-up" exact component= {SignUp}/>
         </Switch>
       </Router>
     </>
